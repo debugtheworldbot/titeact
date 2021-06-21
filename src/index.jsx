24aad3dad6c2c147a86b1a7309eb3cf05035134d
tian <istiancz@gmail.com>
2021-06-21 17:57:43 +0800

    render

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,51 @@
 const createElement = (type, props, ...children) => {
-  return Object.assign({
-    type,
-    props: {
-      ...props,
-      children:children.map(child=>
-        child instanceof Object?child:createTextNode(child)
-      )
-    }
-  },null)
-}
-const createTextNode = (text)=>{
+  return Object.assign(
+    {
+      type,
+      props: {
+        ...props,
+        children: children.map((child) =>
+          child instanceof Object ? child : createTextNode(child)
+        ),
+      },
+    },
+    null
+  );
+};
+const createTextNode = (text) => {
   return {
-    type:"TEXT_ELEMENT",
-    props:{
-      nodeValue:text,
-      children:[]
-    }
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+};
+const render = (element, container) => {
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode(element.props.nodeValue)
+      : document.createElement(element.type);
+  const isProperty = (key) => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((k) => (dom[k] = element.props[k]));
+  for (const child of element.props.children) {
+    render(child, dom);
   }
-}
+  container.appendChild(dom);
+};
 const Titeact = {
-  createElement
-}
+  createElement,
+  render,
+};
 
 /** @jsx Titeact.createElement */
 const element = (
   <div id="foo">
-    <a>bar</a>
-    <b />
+    <h1>bar</h1>
+    <h2>foo</h2>
   </div>
-)
-
-console.log(element);
+);
+const container = document.getElementById("root");
+Titeact.render(element, container);
